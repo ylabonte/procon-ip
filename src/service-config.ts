7809081cdd305c520aa849fbcb21082bf764cee6
Yannic Labonte <yannic.labonte@gmail.com>
2020-10-04 03:40:09 +0200

    Fix service config

--- src/service-config.ts
+++ src/service-config.ts
@@ -5,4 +5,8 @@ export interface ServiceConfig {
   password: string;
   basicAuth: boolean;
   timeout: number;
+
+  // Use a catch-all approach to enable array-like iteration with
+  // key as a variable...
+  [key: string]: any;
 }
