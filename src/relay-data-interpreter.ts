ab216a9319ce4091d7156f122c2b703b84fa66e1
Yannic Labonte <yannic.labonte@gmail.com>
2020-10-16 00:59:58 +0200

    Update docs

--- src/relay-data-interpreter.ts
+++ src/relay-data-interpreter.ts
@@ -1,3 +1,9 @@
+/**
+ * The [[`RelayDataInterpreter`]] is a helper for all the binary operations
+ * that are necessary to read and write relay states.
+ * @packageDocumentation
+ */
+
 import { GetStateCategory, GetStateData } from './get-state-data';
 import { RelayDataObject } from './relay-data-object';
 import { GetStateDataObject } from './get-state-data-object';
@@ -7,6 +13,8 @@ import { Log } from './logger';
  * The relay state is a two bit value in decimal representation:
  * - lsb: 0 = off, 1 = on
  * - msb: 0 = auto, 1 = manual
+ * 
+ * It is used by the [[`RelayDataInterpreter`]] and the [[`UsrcfgCgiService`]].
  */
 export enum RelayStateBitMask {
   on = 1,
@@ -17,87 +25,156 @@ export enum RelayStateBitMask {
   // autoOn = 1
 }
 
+/**
+ * The [[`RelayDataInterpreter`]] is a helper for all the binary operations
+ * that are necessary to read and write relay states.
+ */
 export class RelayDataInterpreter {
-  public byteState!: [number, number];
+  /**
+   * A pair of bit patterns in decimal representation according to the way the
+   * `/usrcfg.cgi` endpoints wants its values.
+   */
+  public bitStates!: [number, number];
+
   private log: Log;
 
+  /**
+   * Initialize a new [[`RelayDataInterpreter`]].
+   * 
+   * @param logger
+   */
   public constructor(logger: Log) {
     this.log = logger;
   }
 
+  /**
+   * Evaluate the current relay states and set the [[`byteState`]] accordingly.
+   * 
+   * The determined values are used to set the switching parameters. Therefore
+   * the input values should be as up-to-date as possible. Otherwise a change
+   * on one relay will reset all other relays based on the `stateData`.
+   * 
+   * **Important**
+   * This method should be called before using any of the set operations
+   * ([[`setOn`]], [[`setOff`]], [[`setAuto`]]).
+   * 
+   * @param stateData The most recent [[`GetStateData`]] instance.
+   */
   public evaluate(stateData: GetStateData): RelayDataInterpreter {
     let relays = stateData.getDataObjectsByCategory(GetStateCategory.RELAYS);
     if (stateData.sysInfo.isExtRelaysEnabled()) {
       relays = relays.concat(stateData.getDataObjectsByCategory(GetStateCategory.EXTERNAL_RELAYS));
-      this.byteState = [65535, 0];
+      this.bitStates = [65535, 0];
     } else {
-      this.byteState = [255, 0];
+      this.bitStates = [255, 0];
     }
     relays.forEach((data: GetStateDataObject) => {
       const relay = new RelayDataObject(data);
       this.log.debug(JSON.stringify(relay));
       /* tslint:disable: no-bitwise */
       if (this.isAuto(relay)) {
-        this.byteState[0] &= ~relay.bitMask;
+        this.bitStates[0] &= ~relay.bitMask;
       }
       if (this.isOn(relay)) {
-        this.byteState[1] |= relay.bitMask;
+        this.bitStates[1] |= relay.bitMask;
       }
       /* tslint:enable: no-bitwise */
     });
     return this;
   }
 
+  /**
+   * True if the given relay ([[`GetStateDataObject`]]) is currently switched
+   * on.
+   * 
+   * @param relay 
+   */
   public isOn(relay: GetStateDataObject): boolean {
     /* tslint:disable: no-bitwise */
     return (relay.raw & RelayStateBitMask.on) === RelayStateBitMask.on;
     /* tslint:enable: no-bitwise */
   }
 
+  /**
+   * True if the given relay ([[`GetStateDataObject`]]) is currently switched
+   * off.
+   * 
+   * @param relay 
+   */
   public isOff(relay: GetStateDataObject): boolean {
     return !this.isOn(relay);
   }
 
+  /**
+   * True if the given relay ([[`GetStateDataObject`]]) is currently not in
+   * auto mode.
+   * 
+   * @param relay 
+   */
   public isManual(relay: GetStateDataObject): boolean {
     /* tslint:disable: no-bitwise */
     return (relay.raw & RelayStateBitMask.manual) === RelayStateBitMask.manual;
     /* tslint:enable: no-bitwise */
   }
 
+  /**
+   * True if the given relay ([[`GetStateDataObject`]]) is currently in auto
+   * mode.
+   * 
+   * @param relay 
+   */
   public isAuto(relay: GetStateDataObject): boolean {
     return !this.isManual(relay);
   }
 
+  /**
+   * Return the appropriate bit patterns for the `/usrcfg.cgi` endpoint to
+   * switch the given relay ([[`GetStateDataObject`]]) on.
+   * 
+   * @param relay 
+   */
   public setOn(relay: GetStateDataObject): [number, number] {
-    this.log.debug(`Relay byte sate: ${JSON.stringify(this.byteState)}`);
+    this.log.debug(`Relay byte sate: ${JSON.stringify(this.bitStates)}`);
     const relayObject = new RelayDataObject(relay);
     /* tslint:disable: no-bitwise */
-    this.byteState[0] |= relayObject.bitMask;
-    this.byteState[1] |= relayObject.bitMask;
+    this.bitStates[0] |= relayObject.bitMask;
+    this.bitStates[1] |= relayObject.bitMask;
     /* tslint:enable: no-bitwise */
 
-    return this.byteState;
+    return this.bitStates;
   }
 
+  /**
+   * Return the appropriate bit patterns for the `/usrcfg.cgi` endpoint to
+   * switch the given relay ([[`GetStateDataObject`]]) off.
+   * 
+   * @param relay 
+   */
   public setOff(relay: GetStateDataObject): [number, number] {
-    this.log.debug(`Relay byte sate: ${JSON.stringify(this.byteState)}`);
+    this.log.debug(`Relay byte sate: ${JSON.stringify(this.bitStates)}`);
     const relayObject = new RelayDataObject(relay);
     /* tslint:disable: no-bitwise */
-    this.byteState[0] |= relayObject.bitMask;
-    this.byteState[1] &= ~relayObject.bitMask;
+    this.bitStates[0] |= relayObject.bitMask;
+    this.bitStates[1] &= ~relayObject.bitMask;
     /* tslint:enable: no-bitwise */
 
-    return this.byteState;
+    return this.bitStates;
   }
 
+  /**
+   * Return the appropriate bit patterns for the `/usrcfg.cgi` endpoint to
+   * switch the given relay ([[`GetStateDataObject`]]) into auto mode.
+   * 
+   * @param relay 
+   */
   public setAuto(relay: GetStateDataObject): [number, number] {
-    this.log.debug(`Relay byte sate: ${JSON.stringify(this.byteState)}`);
+    this.log.debug(`Relay byte sate: ${JSON.stringify(this.bitStates)}`);
     const relayObject = new RelayDataObject(relay);
     /* tslint:disable: no-bitwise */
-    this.byteState[0] &= ~relayObject.bitMask;
-    this.byteState[1] &= ~relayObject.bitMask;
+    this.bitStates[0] &= ~relayObject.bitMask;
+    this.bitStates[1] &= ~relayObject.bitMask;
     /* tslint:enable: no-bitwise */
 
-    return this.byteState;
+    return this.bitStates;
   }
 }
--- src/usrcfg-cgi.service.ts
+++ src/usrcfg-cgi.service.ts
@@ -1,20 +1,51 @@
+/**
+ * The [[`UsrcfgCgiService`]] uses the `/usrcfg.cgi` endpoint of the ProCon.IP
+ * pool controller to switch its relay states. The actual states can be read
+ * from the [[`GetStateService`]].
+ * @packageDocumentation
+ */
+
+import axios, { AxiosPromise, Method } from 'axios';
 import { AbstractService } from './abstract-service';
 import { GetStateService } from './get-state.service';
 import { GetStateData } from './get-state-data';
 import { GetStateDataObject } from './get-state-data-object';
 import { RelayDataInterpreter, RelayStateBitMask } from './relay-data-interpreter';
-import axios, { AxiosPromise, Method } from 'axios';
 import { ServiceConfig } from './service-config';
 import { Log } from './logger';
 
+/**
+ * This enum can be used with the [[`UsrcfgCgiService.setState`]] method. But
+ * there are also shorthand wrappers for all states ([[`setOn`]], [[`setOff`]],
+ * [[`setAuto`]]) that can be used.
+ */
 export enum SetStateValue {
-  ON = 1,
   OFF = 0,
+  ON = 1,
   AUTO = 2,
 }
 
+/**
+ * The [[`UsrcfgCgiService`]] uses the `/usrcfg.cgi` endpoint of the ProCon.IP
+ * pool controller to switch its relay states.
+ * 
+ * It uses two bit patterns in decimal representation, to set on/off and auto
+ * states for all relays at once. This means considering the states of all
+ * relays, those which states should be changed as well as the ones which 
+ * states not gonna to be changed.
+ */
 export class UsrcfgCgiService extends AbstractService {
+  /**
+   * Specific service endpoint.
+   * 
+   * A path relative to the [[`ServiceConfig.controllerUrl`]].
+   */
   public _endpoint = '/usrcfg.cgi';
+
+  /**
+   * HTTP request method for this specific service endpoint.
+   * See: `axios/Method`
+   */
   public _method: Method = 'post';
 
   private stateData: GetStateData;
@@ -23,6 +54,15 @@ export class UsrcfgCgiService extends AbstractService {
 
   private relayDataInterpreter: RelayDataInterpreter;
 
+  /**
+   * Initialize a new [[`UsrcfgCgiService`]]
+   * 
+   * @param config The service config.
+   * @param logger The service logger.
+   * @param getStateService A corresponding [[`GetStateService`]] (must address
+   *                        the same pool controller)
+   * @param relayDataInterpreter An instance of [[`RelayDataInterpreter`]].
+   */
   public constructor(
     config: ServiceConfig,
     logger: Log,
@@ -36,18 +76,39 @@ export class UsrcfgCgiService extends AbstractService {
     this._requestHeaders['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
   }
 
+  /**
+   * Switch the given relay on. 
+   * 
+   * @param relayData Relay data object.
+   */
   public async setOn(relayData: GetStateDataObject): Promise<number> {
     return this.setState(relayData, SetStateValue.ON);
   }
 
+  /**
+   * Switch the given relay off.
+   * 
+   * @param relayData Relay data object.
+   */
   public async setOff(relayData: GetStateDataObject): Promise<number> {
     return this.setState(relayData, SetStateValue.OFF);
   }
 
+  /**
+   * Set the given relay in auto mode.
+   * 
+   * @param relayData Relay data object.
+   */
   public async setAuto(relayData: GetStateDataObject): Promise<number> {
     return this.setState(relayData, SetStateValue.AUTO);
   }
 
+  /**
+   * Set the desired relay state.
+   * 
+   * @param relay Relay data object.
+   * @param state The desired state.
+   */
   private async setState(relay: GetStateDataObject, state: SetStateValue | number): Promise<number> {
     for (let errors = 0; errors < 3; errors++) {
       try {
